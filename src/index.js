d98a90e09af91dd211e60bae7e284f42d8259c13
Muhammad Afzal Balghari <mafzalbalghari101@gmail.com>
2025-11-21 08:47:00 -0800

    connect mongo atlas

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ dotenv.config({
 
 connectDB()
 
+.then( () =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port : ${process.env.PORT}`)
+    })
+}
+)
+
+.catch((err) => {
+    console.log("Mongoes DB connsction failed ", err)
+})
+
 
 /*
 import express from "express";
